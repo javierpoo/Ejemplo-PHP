87abf2fd8a851ce3e23df460962669557d55123a
javierpoo <javier.poo@gmail.com>
2019-07-01 12:32:16 -0400

    Update form.php

--- form.php
+++ form.php
@@ -1,11 +1,52 @@
-<html>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Bootstrap Example</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
+</head>
 <body>
+  
+<div class="container">
+  <h2>Vertical (basic) form</h2>
+  <form action="welcome.php">
+    <div class="form-group">
+      <label for="email">Email:</label>
+      <input type="email" class="form-control" id="email" placeholder="Enter email" name="email">
+    </div>
+    <div class="form-group">
+      <label for="pwd">Password:</label>
+      <input type="password" class="form-control" id="pwd" placeholder="Enter password" name="pwd">
+    </div>
 
-<form action="welcome.php" method="post">
-Name: <input type="text" name="name"><br>
-E-mail: <input type="text" name="email"><br>
-<input type="submit">
-</form>
+    <div class="form-group">
+      <label for="pwd">$trx_id:</label>
+      <input type="text" class="form-control" id="trx_id" placeholder="Enter password" value="<?php echo  rand(1, 1000000); ?>" name="monto">
+    </div>
+    
+    <div class="form-group">
+      <label for="pwd">$monto:</label>
+      <input type="text" class="form-control" id="monto" placeholder="Enter password" value="<?php echo  rand(10000, 100000); ?>" name="monto">
+    </div>
+
+    <div class="form-group">
+    <label for="mediopago1">Medio Pago</label>
+    <select class="form-control" id="mediopago1">
+      <option value="">Ninguno</option>
+      <option value="3">Webpay</option>
+      <option value="10">Ripley</option>
+      <option value="21">BancoEstado Multicanal</option>
+    </select>
+  </div>
+    
+    
+    <button type="submit" class="btn btn-default">Submit</button>
+  
+  </form>
+</div>
 
 </body>
 </html>
