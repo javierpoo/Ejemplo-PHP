3e042af989d20f2e7d0ba53f1f48c9f0c7e09138
javierpoo <javier.poo@gmail.com>
2019-07-01 12:48:00 -0400

    Update welcome.php

--- welcome.php
+++ welcome.php
@@ -1,11 +1,20 @@
-<html>
-<body>
-
-Welcome 
-  
-TRXID <?php echo $_POST["trx_id"]; ?><br>
-Monto: <?php echo $_POST["monto"]; ?><br>
-MedioPago:<?php echo $_POST["mediopago1"]; ?>
-  
-</body>
-</html>
+<?php
+require_once('puntopagos.class.php');
+// A efectos de prueba creamos un id de transaccion y monto aleatorios
+$trx_id = $_POST["trx_id"];
+$monto = $_POST["monto"];
+$mediopago = $_POST["mediopago1"];
+// Llamamos a la API para crear la transaccion. Esto nos devuelve la respuesta con el estado
+// y el valor del token que identificará la transaccion de aquí en mas
+$respuesta = PuntoPagos::CrearTransaccion($trx_id, $monto, $mediopago);
+echo "Respuesta ", var_dump($respuesta);
+if ($respuesta->{'token'} != null){
+    // Esta es la URL a redirigir al cliente para que continue y efectue el pago en el medio que corresponde
+    $url = PUNTOPAGOS_URL."/transaccion/procesar/".$respuesta->{'token'};
+    ob_start();
+    header("Location: $url");
+    ob_flush();
+}
+else{
+    echo $respuesta->{'error'};
+}
