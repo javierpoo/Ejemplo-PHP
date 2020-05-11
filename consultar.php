d0d73b9d1a3e474addc3122b8020b69b92ea68a5
javierpoo <javier.poo@gmail.com>
2020-05-11 13:31:04 -0400

    Create consultar.php

---
+++ consultar.php
@@ -0,0 +1,23 @@
+<?php
+require_once('puntopagos.class.php');
+// A efectos de prueba creamos un id de transaccion y monto aleatorios
+$trx_id = $_POST["trx_id"];
+$monto = $_POST["monto"];
+$mediopago = $_POST["mediopago1"];
+$email = $_POST["email"];
+
+// Llamamos a la API para crear la transaccion. Esto nos devuelve la respuesta con el estado
+// y el valor del token que identificará la transaccion de aquí en mas
+$respuesta = PuntoPagos::ConsultarTransaccion($trx_id, $monto);
+echo "Respuesta ", var_dump($respuesta);
+
+if ($respuesta->{'token'} != null){
+    // Esta es la URL a redirigir al cliente para que continue y efectue el pago en el medio que corresponde
+    $url = PUNTOPAGOS_URL."/transaccion/procesar/".$respuesta->{'token'};
+    ob_start();
+    header("Location: $url");
+    ob_flush();
+}
+else{
+    echo $respuesta->{'error'};
+}
