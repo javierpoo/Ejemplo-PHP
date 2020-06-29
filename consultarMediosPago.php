d42459cd968d0a23b269e2b4e7c1b09a29ee8eb7
javierpoo <javier.poo@gmail.com>
2020-06-28 22:15:54 -0400

    Create consultarMediosPago.php

---
+++ consultarMediosPago.php
@@ -0,0 +1,10 @@
+<?php
+require_once('puntopagos.class.php');
+// A efectos de prueba creamos un id de transaccion y monto aleatorios
+$token = $_POST["token"];
+$trx_id = $_POST["trx_id"];
+$monto = $_POST["monto"];
+
+// Llamamos a la API para consulta la transaccion.
+$respuesta = PuntoPagos::ConsultarMediosPago();
+echo "Respuesta ", var_dump($respuesta);
