c9571184106923127b746219f2e25d39261fe14d
javierpoo <javier.poo@gmail.com>
2020-07-17 13:33:22 -0400

    Update puntopagos.class.php

--- puntopagos.class.php
+++ puntopagos.class.php
@@ -33,12 +33,12 @@ class PuntoPagos {
     * @return array Respuesta
     */
 
-    public static function CapturarTransaccion($trx_id, $monto)
-    {
+    public static function CapturarTransaccion($token, $trx_id, $monto){
         $funcion = 'transaccion/capturar';
         $monto_str = number_format($monto, 2, '.', '');
-        $data = '{"trx_id":"'.$trx_id.'","monto":"'.$monto_str.'"}';
-        $header_array = PuntoPagos::TraerHeader($funcion, $trx_id, $monto_str);
+        $data = '{"trx_id":"'.$trx_id.'","token":"'.$token.'","monto":"'.$monto_str.'"}';
+        $header_array = PuntoPagos::TraerHeaderConsulta($funcion, $token, $trx_id, $monto_str);
+
         return json_decode(PuntoPagos::ExecuteCommand(PUNTOPAGOS_URL.'/'.$funcion, $header_array, $data));
     }
 
